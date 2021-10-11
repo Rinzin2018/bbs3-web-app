ccc060a6fb9e61b3238807e8abaef4382a593da5
Dawa Tsheing <dawa.tshering@dcpl.bt>
2021-10-11 11:38:30 +0600

    Site title

--- src/component/Landing/Layout/Layout.js
+++ src/component/Landing/Layout/Layout.js
@@ -3,6 +3,7 @@ import {ThemeProvider} from '@material-ui/core/styles';
 import {theme} from '../../../Theme';
 import {Footer} from '../Footer/Footer';
 import {Appbar} from '../../Appbar/Appbar';
+import { Helmet } from "react-helmet"
 
 // styles
 const pageStyles = {
@@ -15,6 +16,12 @@ const Layout = ({children}) => {
 
   return (
     <main style={pageStyles}>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>BBS3</title>
+        <link rel="canonical" href="https://bbs3.bt" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Helmet>
       <ThemeProvider theme={theme}>
         <div>
           <Appbar/>
